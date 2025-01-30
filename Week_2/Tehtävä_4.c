81f8e0567efd51c88da80f217b962f08297ebeed
tuuli <tuuli.mrl@gmail.com>
2025-01-30 15:21:46 +0200

    tehtävä 4 vk 2

--- Week_2/Tehtävä_4.c
+++ Week_2/Tehtävä_4.c
@@ -1,6 +1,40 @@
 //
 // Created by tuuli on 29/01/2025.
 //
-int read_integer(void);
+#include <stdio.h>
 
-float average(void);
+int read_integer(int num);
+float average(int sum, int count);
+
+int read_integer(int num){
+  printf("Enter a positive integer to calculate, or negative to stop:\n");
+  if(scanf("%d", &num) != 1) {
+      while(getchar() != '\n');
+      printf("Invalid input, try again\n");
+  }
+  return num;
+}
+
+float average(int sum, int count){
+  float average = sum / (count * 1.0);
+  return average;
+}
+
+int main(){
+    int sum = 0;
+    int count = 0;
+    int num = 0;
+
+    printf("Enter an integers and I shall calculate the average!\n");
+    while (num >= 0){
+        num = read_integer(num);
+        if(num > 0) {
+            sum += num;
+            count++;
+            while(getchar() != '\n');
+        }
+    }
+
+    printf("You entered %d positive numbers. The average is: %.3f \n", count, average(sum, count));
+    return 0;
+}
