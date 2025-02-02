574e3791e366bd9692fb050a0eeaa161fcb9148c
tuuli <tuuli.mrl@gmail.com>
2025-02-02 22:18:57 +0200

    Tehtävä 6, vk 2, kokonaan tehty, (note to self: terminaalin ylimääräisen syötteen tyhjennys (while(getchar()..) pitää olla HETI valinta tarkistuksen jälkeen, tai koodi jää jumiin.. -_-

--- Week_2/Rand_testing.c
+++ Week_2/Rand_testing.c
@@ -5,17 +5,57 @@
 #include <stdlib.h>
 #include <time.h>
 
+/*
 int main(void)
 {
     srand(time(NULL)); // use current time as seed for random generator
     int random_variable = rand();
-    printf("Random value on [0,%d]: %d\n", 6, random_variable);
+    printf("Random value on [0,%d]: %d\n", RAND_MAX, random_variable);
 
     // roll a 6-sided die 20 times
     for (int n=0; n != 20; ++n) {
         int x = 7;
         while(x > 6)
-            x = 1 + rand()/((7 + 1u)/6); // Note: 1+rand()%6 is biased
+            x = 1 + rand()/((RAND_MAX + 1u)/6); // Note: 1+rand()%6 is biased
         printf("%d ",  x);
     }
+}
+
+
+ *srand(time(NULL)); // use current time as seed for random generator
+int random_variable = rand();
+printf("Random value on [0,%d]: %d\n", 6, random_variable);
+
+// roll a 6-sided die 20 times
+for (int n=0; n != 20; ++n) {
+int x = 7;
+while(x > 6)
+x = 1 + rand()/((7 + 1u)/6); // Note: 1+rand()%6 is biased
+printf("%d ",  x);}*/
+
+int heitä_d6() {
+    srand(time(NULL)); // use current time as seed for random generator
+    int random_variable = rand();
+    printf("Random value on [0,%d]: %d\n", 6, random_variable);
+
+    int x = 7;
+    while(x > 6) {
+        x = 1 + rand()/((7 + 1u)/6);
+    }
+    printf("%d ",  x);
+
+}
+
+int heitä_d20() {
+    int x = 21;
+    while(x > 20)
+        x = 1 + rand()/((21 + 1u)/20);
+    printf("%d ",  x);
+}
+
+int main(void)
+{
+    heitä_d6();
+    heitä_d20();
+    return 0;
 }
--- Week_2/Tehtävä_6.c
+++ Week_2/Tehtävä_6.c
@@ -11,37 +11,59 @@ int roll_d20();
 int print_menu();
 
 int print_menu(){
-    printf("Tervetuloa nopanheitto ohjelmaan!\n");
     printf("Valitse seuraavista:\n1. Heitä D6 noppaa.\n2. Heitä D20 noppaa.\n3. Lopeta.\n");
     return 0;
 }
 
 int roll_d6(){
     printf("Heität D6 noppaa!\n");
-    /*srand(time(NULL)); // use current time as seed for random generator
-    int random_variable = rand();
-    printf("Random value on [0,%d]: %d\n", 7, random_variable);
-     */
+    srand(time(NULL));
 
+    int x = 7;
+    while(x > 6) {
+        x = 1 + rand()/((7 + 1u)/6);
+    }
+    printf("Silmäluku: %d \n",  x);
     return 0;
 }
 
 int roll_d20(){
+    srand(time(NULL));
+
     printf("Heität d20 noppaa!\n");
+    int x = 21;
+    while(x > 20) {
+        x = 1 + rand()/((21 + 1u)/20);
+    }
+    printf("Silmäluku:%d \n",  x);
     return 0;
 }
 
 int main(void){
     int ton = 1;
     int choice;
+    printf("Tervetuloa nopanheitto ohjelmaan!\n");
     while(ton == 1){
         print_menu();
         if (scanf("%d", &choice) != 1) {
-            printf("Syötäthän vain numeroita! Valitse 1 Menussa esitetyistä vaihtoehdoista.\n");
             while(getchar() != '\n');
+            printf("Syötäthän vain numeroita! Valitse 1 Menussa esitetyistä vaihtoehdoista.\n");
+        }else if (choice == 1) {
+            roll_d6();
+            while(getchar() != '\n');
+        }else if (choice == 2){
+            while(getchar() != '\n');
+            roll_d20();
+        }else if (choice == 3) {
+            while(getchar() != '\n');
+            ton = 0;
+        }else {
+            while(getchar() != '\n');
+            printf("Syöttämänne valinta ei ole kelvollinen, valitse 1 Menussa esitetyistä vaihtoehdoista.\n");
         }
-        else{
-            switch(choice) {
+
+
+/*            switch(choice) {
                 case 1:
                     roll_d6();
                     while(getchar() != '\n');
@@ -54,12 +76,14 @@ int main(void){
                     ton = 0;
                     while(getchar() != '\n');
                     break;
+                case 4:
+                    printf("Syötäthän vain numeroita! Valitse 1 Menussa esitetyistä vaihtoehdoista.\n");
+                    break;
                 default:
                     printf("Syöttämänne valinta ei ole kelvollinen, valitse 1 Menussa esitetyistä vaihtoehdoista.\n");
                     while (getchar() != 'n');
-                    break;
-            }
+                    break; */
+
         }
-    }
     return 0;
 }
