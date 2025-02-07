8bf7bb47f14b54d58a20c92113b7337fba678f96
tuuli <tuuli.mrl@gmail.com>
2025-02-07 20:01:42 +0200

    Viikolta 3, tehtävä 7 melkein valmis, toimii oikeilla syötteillä mut virheenkäsittely sakkaa, en jaksa väsyttää tireddd

--- Week_3/Tehtävä_7.c
+++ Week_3/Tehtävä_7.c
@@ -18,21 +18,21 @@ bool read_positive(int *value){
 		ton = false;
 	}
 	*/
-	if ((scanf("%d", &number) == 1) && number >0){
-		*value = number;
-		ton = true;
+	if ((scanf("%d", &number) == 1) && number >0){      //To do Joku tässä mättää, ei nappaa else rakenteesta false arvoa ja palauta sitä?
+		*value = number;        //To do, kirjainsyötteistä osaa siirtyä elseen, miinus syötteestä jää jumiin terminaaliin
+		ton = true;     //En jaksaaaaaa väsysleepyheadech just let me sleep goddamnit
 	}else{
 		ton = false;
 		while(getchar() == '\n');
-		printf("Invalid input");
+		printf("Invalid input.\n");
 		}
 
 	return ton;
 }
 
-int arvauspeli(){
-
-
+int arvauspeli(int num){
+	int cheated_no = num * 2 + 20;
+	printf("You didn’t get it right. I have %d euros.\n", cheated_no);
 	return 0;
 }
 
@@ -40,18 +40,19 @@ int main(){
 	int pos_no = 0;
 	int *pos_point = &pos_no;
 	int count = 0;
-	if (read_positive(pos_point) == false){
-		count += 1;
-	}
-	else if(count == 3){
-		//lopeta ohjelma
-		break;
-	}
-	else{
-		//arvauspeli
+	while (true){
+		printf("Guess how much money I have!\n");
+		if (read_positive(pos_point) == false){
+			count += 1;
+		}
+		else if(count == 3){
+			false;		//lopeta ohjelma
+		}
+		else{
+			arvauspeli(pos_no);
+		}
 	}
 
-
-
+	printf("I give up! See you later!");
 	return 0;
 }
