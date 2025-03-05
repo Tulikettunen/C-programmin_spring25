fea25e244e04079664c4732fba22215f0d184286
tuuli <tuuli.mrl@gmail.com>
2025-03-05 11:48:47 +0200

    Viikolta 3, tehtävä 9 valmis.

---
+++ ViLLE_teht/ViLLE_5_1.c
@@ -0,0 +1,48 @@
+//
+// Created by tuuli on 05/03/2025.
+/*You need to implement two functions: binary_reader and digit_counter
+
+Binary reader takes a pointer to unsigned int as parameter and returns boolean value.
+True means that a binary number was successfully read. Call read_line to read strings that are to be converted
+to unsigned integers. Read_line returns true when a line was successfully read and false when
+the end of input is reached.
+
+Each valid input line contains a binary number that starts with prefix "0b". There may be white space
+at beginning of the line before prefix. When a valid prefix is found binary reader reads binary digits
+until it reads a character that is not a '0' or '1'.
+Hint: when a valid digit is found shift the current value to left
+and and set the new value to least significant bit.
+
+Digit counter returns the minimum number of hexadecimal digits that are needed to print the number given as parameter.
+Hint: you need find the leftmost group of 4 bits that have value greater than 0.
+*/
+//
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+/* call this function to read input */
+bool read_line(char *line, size_t size);
+
+/* you need to implement the following two functions */
+bool binary_reader(unsigned int *pu);
+int digit_counter(unsigned int nr);
+
+//
+
+
+
+//
+int main(int arcg, char **argv)
+{
+    unsigned int number = 0;
+
+    while(binary_reader(&number)) {
+        printf("%11u: %08X, %d\n", number, number, digit_counter(number));
+    };
+
+    return 0;
+
+}
--- Week_3/Tehtävä_9.c
+++ Week_3/Tehtävä_9.c
@@ -1,3 +1,77 @@
 //
 // Created by tuuli on 05/02/2025.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define SIZE 20
+#define MIN 1
+#define MAX 20
+
+void print_numbers(const unsigned int *array, int count);
+unsigned int * create_random_list(int min, int max);
+int find_first(const unsigned int *array, unsigned int what);
+int read_what(void);
+
+unsigned int *create_random_list(int min, int max){
+    static unsigned int randomlist[SIZE];        //Luo lista random numeroilla
+    for (int i = 0; i < SIZE; i++){
+        if (i == SIZE - 1){
+            randomlist[i] = 0;
+        }
+        else{
+            randomlist[i] = rand() % (max - min + 1) + min;
+        }
+    }
+    return randomlist;
+}
+
+void print_numbers(const unsigned int *array, int count){
+    for (int i = 0; i< count; i++){
+        printf("%*d\n", 8, array[i]);
+    }
+}
+
+int find_first(const unsigned int *array, unsigned int what){
+    int index = -1;
+    for ( int i = 0; array[i] != 0; i ++){
+        if (array[i] == what){
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
+
+int read_what(void) {
+    int what;
+    while (scanf("%d", &what) != 1 || what < 0 || what > MAX) {
+        while(getchar() != '\n');
+        printf("Invaild input, try again:\n");
+    }
+    return what;
+}
+
+int main(void){
+    const unsigned int *rand_numbs = create_random_list(MIN, MAX);
+    print_numbers(rand_numbs, SIZE);
+
+    bool ton = 1;
+    while (ton){
+        int what = read_what();
+        while(getchar() != '\n');
+        if ( what == 0) {
+            ton = false;
+        }
+        else if (find_first(rand_numbs, what) == -1){
+            printf("Number not found.\n");
+        }
+        else {
+            printf("Index of numer %d you're searching is: %d \n", what, find_first(rand_numbs, what));
+        }
+    }
+    return 0;
+}
+
